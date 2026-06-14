aef4411d7939548849b14087005e50a869be6874
CoYildiz <yusufyildizr@gmail.com>
2026-06-15 00:33:27 +0300

    issue about log file

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # Create a game loop
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pass
+
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
